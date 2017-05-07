8451a4b3b6251293da064d9894be95bf4ff80522
fernandeznico <nicofernandez@alumnos.unc.edu.ar>
2017-05-07 17:51:14 -0300

    Todo traido, se prueba

---
+++ Recursos/Print.h
@@ -0,0 +1,54 @@
+/**
+ * @author Fernández Nicolás (nicofernandez@alumnos.unc.edu.ar)
+ * @date Mayo, 2017
+ *
+ * @brief Manejo de archivos: Lectura, búsqueda de caracteres, etc
+ * 
+ * \file Print.h
+ */
+
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <stdio.h>
+
+void Print_vector_int( int * vector , long unsigned int rows )
+{
+	
+	long unsigned int row;
+	for( row = 0 ; row < rows ; row++ )
+		printf( "\n vector[%lu] = %i" , row , vector[row] );
+	
+}
+
+void Print_matrix_int
+( int ** matrix , long unsigned rows , long unsigned columns )
+{
+	
+	long unsigned int row;
+	long unsigned int column;
+	for( row = 0 ; row < rows ; row++ )
+		for( column = 0 ; column < columns ; column++ )
+			printf( "\n matrix[%lu][%lu] = %i" ,
+					 row ,
+					 column ,
+					 matrix[row][column] );
+	
+}
+
+void Print_matrix_float
+( float ** matrix , long unsigned rows , long unsigned columns )
+{
+	
+	long unsigned int row;
+	long unsigned int column;
+	for( row = 0 ; row < rows ; row++ )
+		for( column = 0 ; column < columns ; column++ )
+			printf( "\n matrix[%lu][%lu] = %f" ,
+					 row ,
+					 column ,
+					 matrix[row][column] );
+	
+}
+
+#endif
